11904b4d5f5c149d9f735793157b5add0d6a84f4
Strangedip <sg02295@gmail.com>
2023-11-09 23:25:22 +0530

    initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ProfileComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
